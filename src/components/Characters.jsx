47a4b2fd9cd5cf7e8093ff0035fbd733c216f4d1
unknown <limonkazakov272007@gmail.com>
2024-01-20 17:55:51 +0600

    add exit button

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,19 +1,39 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Characters = () => {
 
-  // localStorage.getItem('data', JSON.parse(response.data.user))
   const data = JSON.parse(localStorage.getItem('data'))
+
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    
+    if (!data) {
+      navigate('/')
+    }
+  }, [])
   
+  const exitF = () => {
+    localStorage.clear()
+    navigate('/')
+  }
+
   return (
     <div>
       <h1>Данные пользователя</h1>
-      <div>{data.city}</div>
-      <div>{data.fullName}</div>
-      <div>{data.job}</div>
-      <div>{data.language}</div>
-      <div>{data.sex}</div>
+      <div>{data && data.city}</div>
+      <div>{data && data.fullName}</div>
+      <div>{ data && data.job}</div>
+      <div>{data && data.language}</div>
+      <div>{ data && data.sex}</div>
+
+      <div>
+        <input type="button" value='Выйти' onClick={exitF} />
+      </div>
     </div>
+
+    
   )
 }
 
